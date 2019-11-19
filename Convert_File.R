19d6c6b4e444be8cea79df1b44ce76d9863945fd
The Big Cheese <yohan.lefol@gmail.com>
2019-11-19 17:52:40 +0100

    Fixed R conversion
    Still need to fix one last thing, will do tomorrow (November 20th)

--- Convert_File.R
+++ Convert_File.R
@@ -1,6 +1,9 @@
 library(tcltk)
 # We need libraries to read and parse URL
 library(RCurl)
+
+# install.packages("tidyverse")
+library(tidyverse)
 options(RCurlOptions = list(cainfo = system.file("CurlSSL", "cacert.pem", package = "RCurl")))
 
 # Read in file with Gene Symbols
@@ -9,7 +12,9 @@ setwd(directory)
 infile <- file.choose()
 data <- read.csv(infile, header = TRUE, sep=",")
 
+#Check this
 newdata <- data[, c("Gene", "log2FoldChange", "pvalue", "padj")]
+
 names(newdata) <- c("Id", "logFC", "P.Value", "adj.P.Val")
 
 # For each gene, look up the hsa identifiers
@@ -17,18 +22,43 @@ hsa = c()
 for (g in 1:length(newdata[[1]])) {
   query = paste("http://rest.kegg.jp/find/hsa/",newdata$Id[g],sep="")
   result = getURL(query)
-  hsa = c(hsa, strsplit(result,"\t")[[1]][1])
+  if (result=="\n"){
+    hsa = c(hsa,result)
+  }
+  my_list <- strsplit(result,"\n")
+  #For loop checks each element of the list (each line of the website) and checks which line contains the desired gene symbol
+  found_this_many = 0
+  for (i in 1:length(my_list[[1]])){
+    x <- str_replace(my_list[[1]][i],"\t"," ")
+    check_comma_tab <- as.character(str_match (x,as.character(paste("\t",newdata$Id[g],",",sep=""))))
+    check_comma_space <- as.character(str_match (x,as.character(paste(" ",newdata$Id[g],",",sep=""))))
+    check_semicolon_tab <- as.character(str_match (x,as.character(paste("\t",newdata$Id[g],";",sep=""))))
+    check_semicolon_space <- as.character(str_match (x,as.character(paste(" ",newdata$Id[g],";",sep=""))))
+    if (is.na(check_comma_tab)==FALSE | is.na(check_comma_space)==FALSE | is.na(check_semicolon_tab)==FALSE | is.na(check_semicolon_space)==FALSE){
+      hsa = c(hsa, strsplit(x," ")[[1]][1])
+      found_this_many = found_this_many + 1
+      if (found_this_many > 1){
+        newdata<-rbind(newdata,newdata[g,])
+      }
+    }
+  }
 }
 
 # Squash them together
 newdata <- cbind(newdata, hsa)
 names(newdata) <- c("Id", "logFC", "P.Value", "adj.P.Val", "entrez")
 
-# Filter out genes missing hsa ID
-newdata <- newdata[grep("hsa:", hsa), ]
-missing <- newdata[-grep("hsa:", hsa), ]
 
-# Write to file
+# Filter out genes missing hsa ID
+missing <- newdata[-grep("hsa:", hsa), ]
+newdata <- newdata[grep("hsa:", hsa), ]
+
+
+
+#Drop the fifth table (Entrez ID) as it is empty in this dataframe
+missing<- missing[,-5]
+
+#Write to file
 dir.create(file.path(paste(directory, "/Resultats", sep="")))
 setwd(file.path(paste(directory, "/Resultats", sep="")))
 write.table(newdata, file = "convert.txt", row.names=FALSE, col.names=TRUE,sep=",")
