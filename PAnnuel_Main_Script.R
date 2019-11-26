07ded2ecbd0d5310fdb8d7b565a538346b156e23
The Big Cheese <yohan.lefol@gmail.com>
2019-11-26 10:13:57 +0100

    Added gene symbol conversion to main - functional

--- PAnnuel_Main_Script.R
+++ PAnnuel_Main_Script.R
@@ -13,6 +13,11 @@ library(ggplot2)
 library(DESeq2)
 library(apeglm)
 
+#Library for gene symbol conversion to hsa
+library(tcltk)
+library(RCurl)
+library(tidyverse)
+
 #Basically a library call for ROntoTools script
 require(graph)
 require(ROntoTools)
@@ -45,7 +50,7 @@ maindir <- getwd()
 dir.create(file.path(maindir, "Normal_Results"))
 setwd(file.path(maindir, "Normal_Results"))
 
-#Create the function
+#Create the function for Deseq2
 DESeq2_pre_processing <- function(File_1, File_2, variable_condition_1, variable_condition_2, do_MA_plot, do_Volcano_Plot)
 {
   ###################################################################################################################################
@@ -199,11 +204,8 @@ DESeq2_pre_processing <- function(File_1, File_2, variable_condition_1, variable
       with(subset(res, padj<sigthresh ), points(log2FoldChange, -log10(pvalue), pch=20, col="red", ...))
       with(subset(res, abs(log2FoldChange)>lfcthresh), points(log2FoldChange, -log10(pvalue), pch=20, col="orange", ...))
       with(subset(res, padj<sigthresh & abs(log2FoldChange)>lfcthresh), points(log2FoldChange, -log10(pvalue), pch=20, col="green", ...))
-      print("honk")
       x <-subset(res, padj<sigthresh & abs(log2FoldChange)>lfcthresh)
-      print("blarg")
       write.csv(x,file = paste(variable_condition_1,"vs",variable_condition_2,"_RESULTS_VOLCANO.csv"))
-      print("after write")
       return (x)
     },error=function(error_message){
     })
@@ -241,7 +243,86 @@ DESeq2_pre_processing <- function(File_1, File_2, variable_condition_1, variable
   return (pre_processed_file)
 }
 
+#Create the function for gene symbol conversion to HSA
+Gene_Symbol_Conversion <- function(DESeq2_file)
+{
+  options(RCurlOptions = list(cainfo = system.file("CurlSSL", "cacert.pem", package = "RCurl")))
 
-#Call the function
+  data <- DESeq2_file
+  newdata <- data[, c("Gene", "log2FoldChange", "pvalue", "padj")]
+  
+  names(newdata) <- c("Id", "logFC", "P.Value", "adj.P.Val")
+  
+  # For each gene, look up the hsa identifiers
+  hsa = c()
+  for (g in 1:length(newdata[[1]])) {
+    query = paste("http://rest.kegg.jp/find/hsa/",newdata$Id[g],sep="")
+    result = getURL(query)
+    if (result=="\n"){
+      hsa = c(hsa,result)
+    }
+    my_list <- strsplit(result,"\n")
+    #For loop checks each element of the list (each line of the website) and checks which line contains the desired gene symbol
+    found_this_many = 0
+    for (i in 1:length(my_list[[1]])){
+      x <- str_replace(my_list[[1]][i],"\t"," ")
+      check_comma_tab <- as.character(str_match (x,as.character(paste("\t",newdata$Id[g],",",sep=""))))
+      check_comma_space <- as.character(str_match (x,as.character(paste(" ",newdata$Id[g],",",sep=""))))
+      check_semicolon_tab <- as.character(str_match (x,as.character(paste("\t",newdata$Id[g],";",sep=""))))
+      check_semicolon_space <- as.character(str_match (x,as.character(paste(" ",newdata$Id[g],";",sep=""))))
+      if (is.na(check_comma_tab)==FALSE | is.na(check_comma_space)==FALSE | is.na(check_semicolon_tab)==FALSE | is.na(check_semicolon_space)==FALSE){
+        if (found_this_many == 0){
+          hsa = c(hsa, strsplit(x," ")[[1]][1]) 
+        }
+        found_this_many = found_this_many + 1
+        if (found_this_many > 0){
+          newdata<-rbind(newdata,newdata[g,])
+          hsa = c(hsa, strsplit(x," ")[[1]][1])
+        }
+      }
+    }
+  }
+  
+  # Squash them together
+  newdata <- cbind(newdata, hsa)
+  names(newdata) <- c("Id", "logFC", "P.Value", "adj.P.Val", "entrez")
+  
+  
+  # Filter out genes missing hsa ID
+  missing <- newdata[-grep("hsa:", hsa), ]
+  newdata <- newdata[grep("hsa:", hsa), ]
+  
+  
+  
+  #Drop the fifth table (Entrez ID) as it is empty in this dataframe
+  missing<- missing[,-5]
+  
+  #Write to file
+  directory <- getwd()
+  dir.create(file.path(paste(directory, "/Resultats_Conversion", sep="")))
+  setwd(file.path(paste(directory, "/Resultats_Conversion", sep="")))
+  write.table(newdata, file = "convert.txt", row.names=FALSE, col.names=TRUE,sep=",")
+  write.table(missing, file = "missing.txt", row.names=FALSE, col.names=TRUE,sep=",")
+  
+  return(newdata)
+}
+
+
+#Call the functions
 DESeq2_file <- DESeq2_pre_processing(File_1, File_2, variable_condition_1, variable_condition_2, do_MA_plot, do_Volcano_Plot)
+
+converted_file <- Gene_Symbol_Conversion(DESeq2_file)
+
+
+
+
+
+
+
+
+
+
+
+
+
 
